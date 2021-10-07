e5a5fa27469f2bf7626739eedd2c8b0b0cdaada6
titanium25 <idpm7N95tLPLskg>
2021-10-07 21:28:30 +0300

    init

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Dashboard from "./Dashboard";
+import Attractions from "./Attractions";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+
+function App() {
+
+
+    return (
+        <Router>
+            <Switch>
+                <Route exact path='/' component={Dashboard}/>
+                <Route exact path='/attractions' component={Attractions}/>
+            </Switch>
+        </Router>
+    )
+}
+
+export default App;
